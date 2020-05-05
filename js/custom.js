8194f2837902adac29be58196cd1a14a4921dbf7
Claire <myokalyar282@gmail.com>
2020-05-05 11:11:11 +0630

    MSU by Kalyar

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+
+
+var coment = [
+{
+	"name":"mgmg", "date":"12 Apr 2020" , "body":"coment1";
+}];
+
+for (var i=0;i<coment.length; i++) {
+	var html = "<div class='comentBox'><div class='leftPanelimg'><'img src='images/banners/b1.jpg"; 
+	$('#container').append(html); 
+}
+});
+
+
